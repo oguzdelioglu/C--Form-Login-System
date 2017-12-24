5106bda3a64d15de038e43d963b6e212534d2104
oguzdelioglu <kemerkaya@hotmail.com.tr>
2017-12-24 03:19:52 +0300

    SQL login System

--- WindowsFormsApplication1/Form1.Designer.cs
+++ WindowsFormsApplication1/Form1.Designer.cs
@@ -30,13 +30,15 @@
         {
             this.button1 = new System.Windows.Forms.Button();
             this.label1 = new System.Windows.Forms.Label();
-            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.password = new System.Windows.Forms.TextBox();
             this.button2 = new System.Windows.Forms.Button();
+            this.username = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
             this.SuspendLayout();
             // 
             // button1
             // 
-            this.button1.Location = new System.Drawing.Point(26, 32);
+            this.button1.Location = new System.Drawing.Point(26, 84);
             this.button1.Name = "button1";
             this.button1.Size = new System.Drawing.Size(75, 23);
             this.button1.TabIndex = 0;
@@ -47,23 +49,23 @@
             // label1
             // 
             this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(23, 9);
+            this.label1.Location = new System.Drawing.Point(27, 44);
             this.label1.Name = "label1";
             this.label1.Size = new System.Drawing.Size(56, 13);
             this.label1.TabIndex = 1;
             this.label1.Text = "Password:";
             // 
-            // textBox1
+            // password
             // 
-            this.textBox1.Location = new System.Drawing.Point(78, 6);
-            this.textBox1.Name = "textBox1";
-            this.textBox1.PasswordChar = '*';
-            this.textBox1.Size = new System.Drawing.Size(100, 20);
-            this.textBox1.TabIndex = 2;
+            this.password.Location = new System.Drawing.Point(82, 41);
+            this.password.Name = "password";
+            this.password.PasswordChar = '*';
+            this.password.Size = new System.Drawing.Size(100, 20);
+            this.password.TabIndex = 2;
             // 
             // button2
             // 
-            this.button2.Location = new System.Drawing.Point(107, 32);
+            this.button2.Location = new System.Drawing.Point(107, 84);
             this.button2.Name = "button2";
             this.button2.Size = new System.Drawing.Size(75, 23);
             this.button2.TabIndex = 3;
@@ -71,16 +73,35 @@
             this.button2.UseVisualStyleBackColor = true;
             this.button2.Click += new System.EventHandler(this.button2_Click);
             // 
+            // username
+            // 
+            this.username.Location = new System.Drawing.Point(82, 12);
+            this.username.Name = "username";
+            this.username.Size = new System.Drawing.Size(100, 20);
+            this.username.TabIndex = 5;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(27, 15);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(58, 13);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "Username:";
+            // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(198, 63);
+            this.ClientSize = new System.Drawing.Size(198, 119);
+            this.Controls.Add(this.username);
+            this.Controls.Add(this.label2);
             this.Controls.Add(this.button2);
-            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.password);
             this.Controls.Add(this.label1);
             this.Controls.Add(this.button1);
             this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
             this.Text = "Form1";
             this.ResumeLayout(false);
             this.PerformLayout();
@@ -91,8 +112,10 @@
 
         private System.Windows.Forms.Button button1;
         private System.Windows.Forms.Label label1;
-        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox password;
         private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.TextBox username;
+        private System.Windows.Forms.Label label2;
     }
 }
 
--- WindowsFormsApplication1/Form1.cs
+++ WindowsFormsApplication1/Form1.cs
@@ -7,26 +7,64 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
-
+using MySql.Data.MySqlClient;
 namespace WindowsFormsApplication1
 {
     public partial class Form1 : Form
     {
+        private string conn;
+        private MySqlConnection connect;
         public Form1()
         {
             InitializeComponent();
         }
-
-        private void button1_Click(object sender, EventArgs e)
+        private void dbconnect()
         {
-            if(textBox1.Text=="steemit")
+            try
             {
-                MessageBox.Show("Login Successful");
+                conn = "Server=localhost;Database=teststeemit;Uid=root;Pwd=;";
+                connect = new MySqlConnection(conn);
+                connect.Open();
+            }
+            catch (MySqlException e)
+            {
+                throw;
+            }
+        }
+        private bool checklogin(string user, string pass)
+        {
+            dbconnect();
+            MySqlCommand cmd = new MySqlCommand();
+            cmd.CommandText = "Select * from accounts where username=@user and password=@pass";
+            cmd.Parameters.AddWithValue("@user", user);
+            cmd.Parameters.AddWithValue("@pass", pass);
+            cmd.Connection = connect;
+            MySqlDataReader login = cmd.ExecuteReader();
+            if (login.Read())
+            {
+                connect.Close();
+                return true;
             }
             else
             {
-                MessageBox.Show("Login Problem.");
+                connect.Close();
+                return false;
             }
+        }
+        private void button1_Click(object sender, EventArgs e)
+        {
+            string user = username.Text;
+            string pass = password.Text;
+            if (user == "" || pass == "")
+            {
+                MessageBox.Show("Empty Fields Detected ! Please fill up all the fields");
+                return;
+            }
+            bool r = checklogin(user, pass);
+            if (r)
+                MessageBox.Show("Correct Login Credentials");
+            else
+                MessageBox.Show("Incorrect Login Credentials");
         }
 
         private void button2_Click(object sender, EventArgs e)
